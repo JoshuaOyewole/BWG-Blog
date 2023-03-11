7a5bec5dcd2e9e3ba6030c9ab1c075f70c1eb7da
Joshua Oyewole <joshpp2013@gmail.com>
2023-03-11 16:37:32 +0100

    changed the Newsletter cta text

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -36,15 +36,18 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
 
   return (
     <div>
-      <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</div>
-      <form className="flex flex-col sm:flex-row" onSubmit={subscribe}>
-        <div>
+      <div className="pb-1 text-xl font-semibold text-gray-800 dark:text-gray-100">{title}</div>
+      <p className="pb-4 text-base">
+        Stay Up-to-Date with the Latest Web Development Trends and Techniques!
+      </p>
+      <form className="flex flex-col sm:w-full sm:flex-row" onSubmit={subscribe}>
+        <div className="w-full lg:basis-4/5">
           <label className="sr-only" htmlFor="email-input">
             Email address
           </label>
           <input
             autoComplete="email"
-            className="focus:blue-600 w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 dark:bg-black"
+            className="focus:blue-600  w-full rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 dark:bg-black"
             id="email-input"
             name="email"
             placeholder={subscribed ? "You're subscribed !  🎉" : 'Enter your email'}
@@ -54,7 +57,7 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
             disabled={subscribed}
           />
         </div>
-        <div className="mt-2 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3">
+        <div className="mt-2 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3 lg:basis-1/5">
           <button
             className={`w-full rounded-md bg-blue-500 py-2 px-4 font-medium text-white sm:py-0 ${
               subscribed ? 'cursor-default' : 'hover:bg-blue-700 dark:hover:bg-blue-400'
